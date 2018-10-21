21309ee8991a3f2aee39fc131773731f7004dde7
Malik Perry <Malikap14@gmail.com>
2018-10-20 17:06:41 -0700

    Create Gig model.

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+    }
+}

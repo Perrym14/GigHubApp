05e1463f6c8210440e6860dcf249cf09c2846b7c
Malik Perry <Malikap14@gmail.com>
2018-10-26 10:57:39 -0700

    Add name to artist

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()

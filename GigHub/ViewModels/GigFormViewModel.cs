59bf0cc80cc2082b29c26c361799c163eabc5ec6
Malik Perry <Malikap14@gmail.com>
2018-10-29 08:47:41 -0700

    Implemented Edit a gig.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,12 +1,16 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
         [Required]
         public string Venue { get; set; }
 
@@ -21,7 +25,25 @@ namespace GigHub.ViewModels
         [Required]
         public byte Genre { get; set; }
 
+        public string Heading { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Action
+        {
+
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update =
+                    (c => c.Update(this));
+
+                Expression<Func<GigsController, ActionResult>> create =
+                    (c => c.Update(this));
+
+                var action = (Id != 0) ? update : create;
+                return (action.Body as MethodCallExpression).Method.Name;
+            }
+        }
 
 
         public DateTime GetDateTime()

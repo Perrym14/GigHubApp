3ffd91e89c7b5d69aa65e7c4092c84af0d111ced
Malik Perry <Malikap14@gmail.com>
2018-11-14 16:40:31 -0800

    Started unit test for CancelGigsController

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using GigHub.Controllers.Api;
+using GigHub.Core.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private CancelGigsController _controller;
+
+        public GigsControllerTests()
+        {
+            var identity = new GenericIdentity("user1@domain.com");
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "user1@domain.com"));
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "1"));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockUoW = new Mock<IUnitOfWork>();
+
+            _controller = new CancelGigsController(mockUoW.Object);
+            _controller.User = principal;
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GigHub.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("GigHub.Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b509b6b-ace7-4eb5-aa54-ac14896b323c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
